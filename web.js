5c13bb52effe76046750aae17498f05706fa7831
thermalpaste <kunalthaggarse@gmail.com>
2013-07-10 04:30:18 +0000

    HW3 First Attempt

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var read = fs.readFileSync("index.html", 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(read);
 });
 
 var port = process.env.PORT || 5000;
